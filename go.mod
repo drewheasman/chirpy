e17885d9e982d531aa0d14874ff34620c7a157ce
Drew Heasman <drewheasman@gmail.com>
2025-02-22 08:03:30 +0000

    serve index

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/drewheasman/chirpy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server.ListenAndServe()
+}
